9419ead53aa4dd1c58c06cd522e750353b08c260
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2024-01-11 19:17:09 -0500

    Finished_Properties

--- AgileProjectGameFinder/GameFinder.Data/Enums/Genre.cs
+++ AgileProjectGameFinder/GameFinder.Data/Enums/Genre.cs
@@ -2,6 +2,16 @@ namespace GameFinder.Data.Enums
 {
     public enum Genre
     {
-        
+        Horror = 1,
+        Indie,
+        Sandbox,
+        Shooter,
+        Simulation,
+        Action_Adventure,
+        Mulitplayer_Online_Battle_Arena,
+        Role_Playing_Game,
+        RealTImeStrat,
+        Sports,
+        Party,
     }
 }
